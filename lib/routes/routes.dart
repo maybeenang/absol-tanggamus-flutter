a74c7bfde7e16fe57b9843995cd7b475aa8d4378
maybeenang <muhammad.120140194@student.itera.ac.id>
2023-09-18 04:58:19 +0700

    feature: interface done

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,22 @@
 import 'package:absensitanggamus/pages/home_page.dart';
 import 'package:absensitanggamus/pages/login_page.dart';
+import 'package:absensitanggamus/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 part 'routes.g.dart';
+
+@TypedGoRoute<SplashRoute>(path: SplashRoute.path)
+class SplashRoute extends GoRouteData {
+  const SplashRoute();
+
+  static const path = '/splash';
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const SplashScreen();
+  }
+}
 
 @TypedGoRoute<HomeRoute>(path: HomeRoute.path)
 class HomeRoute extends GoRouteData {
